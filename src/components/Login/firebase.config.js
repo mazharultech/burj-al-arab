d708366d73529f398673ad27f30255b6b6280244
mazharul99tech <mazharul99tech@gmail.com>
2020-10-26 00:32:54 +0600

    burj-al-arab authentication added

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDC5w9ocO0noK76DJuXqmPSHQ0j27_Qh44",
+    authDomain: "burj-al-arab-fa332.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-fa332.firebaseio.com",
+    projectId: "burj-al-arab-fa332",
+    storageBucket: "burj-al-arab-fa332.appspot.com",
+    messagingSenderId: "1061248853927",
+    appId: "1:1061248853927:web:6da21ec28eb88cf3043882"
+  };
+
+  export default firebaseConfig;
